a264249ffe31af70e215e4bbcba5356c5a7eb1a3
admin <admin@MEDION>
2016-06-28 21:47:49 +0200

    Changed World to Azure

--- src/SampleSite/Program.fs
+++ src/SampleSite/Program.fs
@@ -9,5 +9,5 @@ open System.Net
 [<EntryPoint>]
 let main [| port |] =
     let config = { defaultConfig with bindings = [ HttpBinding.mk HTTP IPAddress.Loopback (uint16 port) ] }
-    startWebServer config (warbler (fun _ -> OK (sprintf "%O: Hello World from Suave running on Azure Websites in a full VS project! No scripts here. Running on port %s." DateTime.UtcNow port)))
+    startWebServer config (warbler (fun _ -> OK (sprintf "%O: Hello Azure from Suave running on Azure Websites in a full VS project! No scripts here. Running on port %s." DateTime.UtcNow port)))
     0
