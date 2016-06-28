4faa94a7d0443d97b164630620449f7de3054100
admin <admin@MEDION>
2016-06-28 18:09:24 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple attempt to get full Suave .NET projects working in Azure Websites without scripts etc.
---
+++ src/SampleSite/Program.fs
@@ -0,0 +1,13 @@
+﻿open Suave.Http.Successful
+open Suave.Web
+open Suave.Http
+open Suave.Types
+open System
+open System.Net
+
+//If running locally, make sure you you supply a port number to the executable.
+[<EntryPoint>]
+let main [| port |] =
+    let config = { defaultConfig with bindings = [ HttpBinding.mk HTTP IPAddress.Loopback (uint16 port) ] }
+    startWebServer config (warbler (fun _ -> OK (sprintf "%O: Hello World from Suave running on Azure Websites in a full VS project! No scripts here. Running on port %s." DateTime.UtcNow port)))
+    0
